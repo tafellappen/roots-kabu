35669776398a075a3495144f78005fd08876e967
Miranda <mauriemma12@gmail.com>
2023-02-04 18:12:33 -0500

    removed unnecessary movement code from player

--- roots-kabu/Assets/Scripts/Player.cs
+++ roots-kabu/Assets/Scripts/Player.cs
@@ -44,7 +44,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        invincibilityCountdown = invincibilityFrames;
+        //invincibilityCountdown = invincibilityFrames;
         positionDiff =  mainCamera.transform.position - transform.position;
     }
 
@@ -52,7 +52,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //make a new vector and only copy the up-down position, we dont want camera to follow the player left-right
         mainCamera.transform.position = transform.position + positionDiff;
         invincibilityCountdown -= 1;
 
@@ -69,91 +69,16 @@ public class Player : MonoBehaviour
         }
 
 
-
-        //animator.SetBool("run", horizontalInput != 0);
-        //animator.SetBool("grounded", isGrounded());
-
-        if (wallJumpCD > 0.2f)
-        {
-
-            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-            if (onWall() && !isGrounded())
-            {
-                body.gravityScale = 0;
-                body.velocity = Vector2.zero;
-            }
-            else
-            {
-                body.gravityScale = 2.5f;
-                ;
-            }
-            if (Input.GetKey(KeyCode.Space))
-            {
-                Jump();
-            }
-        }
-        else
-        {
-            wallJumpCD += Time.deltaTime;
-        }
     }
 
-    public void TakeDamage(int amount)
-    {
-        if (invincibilityCountdown <= 0)
-        {
-            health -= amount;
-            invincibilityCountdown = invincibilityFrames;
 
-        }
-
-        //Debug.Log(health);
-        //Debug.Log(invincibilityCountdown);
-        //todo: check if player dead and end game somehow
-    }
-
-    private void Jump()
-    {
-        if (isGrounded())
-        {
-            body.velocity = new Vector2(body.velocity.x, jumpPower);
-            //animator.SetTrigger("jump");
-        }
-        else if (onWall() && !isGrounded())
-        {
-
-            if (horizontalInput == 0)
-            {
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
-                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            }
-            else
-            {
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
-            }
-            wallJumpCD = 0;
-        }
-
-
-    }
 
     private void OnCollisionEnter2D(Collision2D collision) //this will not run unless IsTrigger is true in the collider (which disables other collision like with the ground, so we probably dont want to do that)
     {
-        if (collision.gameObject.tag == "Ground")
-        {
-        }
+        //if (collision.gameObject.tag == "Ground")
+        //{
+        //}
     }
 
-    private bool isGrounded()
-    {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        return raycastHit.collider != null;
-    }
 
-    private bool onWall()
-    {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0,
-            new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
-        return raycastHit.collider != null;
-    }
 }
