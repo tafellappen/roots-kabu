ad8a1afacec5547ebeb3d4cbafb02bc9273af684
Miranda <mauriemma12@gmail.com>
2023-02-04 19:21:45 -0500

    added scene manager and a move upwards script

---
+++ roots-kabu/Assets/Scripts/MoveUpwards.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Everything that isnt the player should be moving upwards, to give the illusion of downward movement
+/// </summary>
+public class MoveUpwards : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
