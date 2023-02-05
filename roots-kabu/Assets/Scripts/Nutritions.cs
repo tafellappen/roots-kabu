72165cb965e15e366653fb6b8b9cebd6232dc09a
Miranda <mauriemma12@gmail.com>
2023-02-05 12:59:40 -0500

    implemented bgm and fixed sfx

--- roots-kabu/Assets/Scripts/Nutritions.cs
+++ roots-kabu/Assets/Scripts/Nutritions.cs
@@ -6,12 +6,18 @@ public class Nutritions : MonoBehaviour
 {
     [SerializeField] float baseHealth = 0.05f;
     float randomScale = 1;
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         randomScale = Random.Range(100, 200) / 100.0f;
-        Debug.Log(randomScale);
+        //Debug.Log(randomScale);
         transform.localScale *= randomScale;
+
+
+        //we cant have the audio source on this gameobject, because when it gets deleted then it will just not play. Just put it on camera for now
+        audioSource = GameObject.Find("Main Camera").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -32,14 +38,13 @@ public class Nutritions : MonoBehaviour
                 HealthBar healthBar = obj.GetComponent<HealthBar>();
                 healthBar.health += baseHealth * randomScale;
 
-                AudioSource audioSource = GetComponent<AudioSource>();
                 audioSource.Play();
                 if (healthBar.health > 1)
                 {
                     healthBar.health = 1.0f;
                 }
             }
-                
+
 
             Destroy(this.gameObject);
         }
